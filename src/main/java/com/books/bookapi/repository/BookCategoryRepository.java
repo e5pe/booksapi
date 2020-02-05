dc76174f8c87fcfaa068575adee41acac740a49f
Esperanza Cutillas Rastoll <especr101@gmail.com>
2020-02-05 15:58:08 +0100

    API para los libros y categorías terminada.

--- src/main/java/com/books/bookapi/repository/BookCategoryRepository.java
+++ src/main/java/com/books/bookapi/repository/BookCategoryRepository.java
@@ -1,11 +1,13 @@
 package com.books.bookapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import com.books.bookapi.model.BookCategory; 
 
 @Repository
+@RepositoryRestResource(collectionResourceRel = "bookCategory", path = "bookCategory")
 public interface BookCategoryRepository extends JpaRepository<BookCategory, Long>{
 
 }
