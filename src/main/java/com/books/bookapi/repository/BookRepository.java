1c3ec74b1f8b1a260d1e92c665c8923d53ad8c5a
Esperanza Cutillas Rastoll <especr101@gmail.com>
2020-03-05 19:33:46 +0100

    Changed rest method.

--- src/main/java/com/books/bookapi/repository/BookRepository.java
+++ src/main/java/com/books/bookapi/repository/BookRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
@@ -12,5 +13,6 @@ import com.books.bookapi.model.Book;
 @Repository
 @CrossOrigin(origins = "http://localhost:4200")
 public interface BookRepository extends JpaRepository<Book, Long> {
+	@RestResource(path = "categoryid")
 	Page<Book> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
